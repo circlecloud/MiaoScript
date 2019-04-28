565419a2929523fa727c0e3277bd61f0fbb6c950
MiaoWoo <admin@yumc.pw>
2019-04-28 19:18:53 +0800

    feat: optimize item lib
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- src/main/resources/plugins/bukkit/WorldEdit.js
+++ src/main/resources/plugins/bukkit/WorldEdit.js
@@ -3,6 +3,7 @@
  * WorldEdit 插件
  */
 /*global Java, base, module, exports, require*/
+var item = require('/api/item');
 var command = require('api/command');
 var Material = Java.type("org.bukkit.Material");
 
@@ -28,11 +29,11 @@ function enable() {
             }
             var player = sender;
             var location = player.location;
-            var type = Material[args[0]] || Material['STONE'];
+            var type = item.type(args[0], 'STONE');
             player.velocity = player.velocity.setY(0.5);
             setTimeout(function() {
                 location.block.type = type
-            }, 8);
+            }, 6);
             return true;
         },
         tab: function(sender, command, args) {
