00287ff80ae81eaf811340675172a6ca652b5569
MiaoWoo <admin@yumc.pw>
2020-02-02 02:16:05 +0800

    feat: optimize ployfill
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- src/main/resources/core/ployfill.js
+++ src/main/resources/core/ployfill.js
@@ -7,17 +7,12 @@
         // Init Global Value
         global.root = root;
         global.logger = logger;
-        // Init Global Function
-        global.noop = global.engineDisable = function() { };
-        global.engineLoad = load;
-        global.load = load = function __PreventGlobalLoadFunction__() {
-            throw new Error('Internal engine system not allow use `load` function!');
-        }
         global.NashornEngineStartTime = new Date().getTime()
+        global.engineLoad = load;
+        global.noop = global.engineDisable = engineDisable = function() { };
+        global.load = load = function __PreventGlobalLoadFunction__() { throw new Error('Internal engine system not allow use `load` function!'); }
+        global.setGlobal = function(key, value) { global[key] = value; };
         // Init console and require
-        global.setGlobal = function(key, value) {
-            global[key] = value;
-        }
         global.console = engineLoad(global.root + '/core/console.js')(logger);
         global.require = engineLoad(global.root + '/core/require.js')(root);
         require('@ms/ployfill')
